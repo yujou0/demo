708430bd28e31dddfa3e669ca29f91b870290fb5
yujou0 <abc83327@gmail.com>
2021-03-19 10:45:28 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      test for demo
+      test for demo 0319
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
